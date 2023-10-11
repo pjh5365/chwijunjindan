5dad8ecc115ceb1cc187ca3a72dd169728282b8f
pjh5365 <pjh5365@naver.com>
2023-10-11 19:32:10 +0900

    Init: 프로젝트 생성
    프로젝트 진행을 위해 필요한 의존성을 넣어 프로젝트를 생성하였다.
    추가한 의존성 목록
    - Spring Web
    - H2 Database
    - Spring security
    - Spring Data JPA
    - Thymeleaf
    - Lombok

---
+++ src/main/java/pulleydoreurae/chwijunjindan/ChwijunjindanApplication.java
@@ -0,0 +1,13 @@
+package pulleydoreurae.chwijunjindan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChwijunjindanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChwijunjindanApplication.class, args);
+    }
+
+}
