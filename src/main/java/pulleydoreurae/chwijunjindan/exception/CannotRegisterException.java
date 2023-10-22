dff46ab0633d54572541fef3663e9b3bd9e7133d
pjh5365 <pjh5365@naver.com>
2023-10-22 19:51:31 +0900

    Feat: 회원가입에 대한 서비스 작성
    회원가입을 위한 서비스를 작성하였다.
    폼 형태로 들어오는 사용자 정보를 받아 회원에 대한 중복을 확인하였고 중복이 발생하면 예외를 발생시키도록 하였다.
    회원 중복을 위한 커스텀 예외를 작성하였다.
    
    Related to: #1

---
+++ src/main/java/pulleydoreurae/chwijunjindan/exception/CannotRegisterException.java
@@ -0,0 +1,7 @@
+package pulleydoreurae.chwijunjindan.exception;
+
+public class CannotRegisterException extends Exception {
+    public CannotRegisterException(String msg) {
+        super(msg);
+    }
+}
